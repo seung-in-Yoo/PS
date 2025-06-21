5dc90d088dff9621225048b109576d2c43f3f2dc
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2025-06-21 21:31:13 +0900

    [Silver IV] Title: 베스트셀러, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine()); 
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String title = sc.nextLine();
+            map.put(title, map.getOrDefault(title, 0) + 1);
+        }
+
+        String bestSeller = "";
+        int maxCount = 0;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String title = entry.getKey();
+            int count = entry.getValue();
+
+            if (count > maxCount) {
+                maxCount = count;
+                bestSeller = title;
+            } else if (count == maxCount) {
+                if (title.compareTo(bestSeller) < 0) {
+                    bestSeller = title;
+                }
+            }
+        }
+
+        System.out.println(bestSeller);
+    }
+}
