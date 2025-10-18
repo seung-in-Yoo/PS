9297b41488074d5020b3c79af7d175db4ccad437
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2025-10-19 01:34:03 +0900

    [Gold IV] Title: 플로이드, Time: 1144 ms, Memory: 240852 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    static final int INF = 1_000_000_000; 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 도시 수
+        int m = sc.nextInt(); // 버스 수
+
+        int[][] dist = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0; 
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int cost = sc.nextInt(); // 간선 
+
+            dist[a][b] = Math.min(dist[a][b], cost); 
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("0 ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
